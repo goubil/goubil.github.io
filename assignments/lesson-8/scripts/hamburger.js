bbb5eea1fb34f299babe74770c1cfd053e1e68e1
goubil <35411274+goubil@users.noreply.github.com>
2018-03-04 00:24:21 -0500

    update

--- assignments/lesson-8/scripts/hamburger.js
+++ assignments/lesson-8/scripts/hamburger.js
@@ -1,5 +1,11 @@
-function toggleHam() {
-    "use strict";
-	document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("primaryNavigation");
+    if (x.className === "show") {
+        x.className = "hide";
+    }
+    else {
+        x.className = "show";
+    }
 }
 
