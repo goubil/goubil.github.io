9e6ffdf9daa980f2b03985994c6c73089dde01e0
goubil <35411274+goubil@users.noreply.github.com>
2018-03-03 23:28:23 -0500

    UPDATE

--- assignments/franklin-7/scripts/windchill.js
+++ assignments/franklin-7/scripts/windchill.js
@@ -1,8 +1,12 @@
-function windChill(windspeed, highTemp, lowTemp) {
-    "use strict";
-    var temp = (highTemp + lowTemp) / 2;
-    var s = Math.pow(windspeed, 0.16);
-    var calcWindChill = 35.74 + (0.6215 * temp) - (35.75 * s) + (0.4275 * temp * s);
-    calcWindChill = calcWindChill.toFixed(0);
-    return calcWindChill;
-}
+var h = parseFloat(document.getElementById("hightemp").innerHTML);
+var l = parseFloat(document.getElementById("lowtemp").innerHTML);
+var avgtemp = (l+h)/2;
+
+var ws = parseFloat(document.getElementById("windspeed").innerHTML);
+
+var final =  35.74 + (0.6215 * avgtemp) - (35.75 * Math.pow(ws,0.16)) + (0.4275 * avgtemp) * Math.pow(ws,0.16);
+
+final = final.toFixed(2);
+
+document.getElementById("windchill").innerHTML = final;
+
