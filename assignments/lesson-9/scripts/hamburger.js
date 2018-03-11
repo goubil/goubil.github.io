e1cddf98849266e5df4a402f18a0a39cc946d7c3
goubil <35411274+goubil@users.noreply.github.com>
2018-03-11 12:23:16 -0400

    update

--- assignments/lesson-9/scripts/hamburger.js
+++ assignments/lesson-9/scripts/hamburger.js
@@ -1,11 +1,4 @@
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function myFunction() {
-    var x = document.getElementById("primaryNavigation");
-    if (x.className === "show") {
-        x.className = "hide";
-    }
-    else {
-        x.className = "show";
-    }
+function toggleNavMenu(){
+    //console.log(document.getElementById("primaryNav").classList);
+    document.getElementById("primaryNav").classList.toggle("hide");
 }
-
--- assignments/lesson-9/scripts/responsiveMenu.js
+++ assignments/lesson-9/scripts/responsiveMenu.js
@@ -1,9 +1,7 @@
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function myFunction() {
-    var x = document.getElementById("primaryNavigation");
+ $(document).ready(function(){
+   document.getElementById("primaryNavigation").innerHTML = "Paragraph changed.";
     if (x.className === "show") {
         x.className = "hide";
     } else {
         x.className = "show";
-    }
-}
