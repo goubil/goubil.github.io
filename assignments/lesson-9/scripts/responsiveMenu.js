c423136ea40038de57a4cdac5d01560f9df458de
goubil <35411274+goubil@users.noreply.github.com>
2018-03-11 19:47:21 -0400

    update

--- assignments/lesson-9/scripts/responsiveMenu.js
+++ assignments/lesson-9/scripts/responsiveMenu.js
@@ -1,7 +1,8 @@
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
- $(document).ready(function(){
+}function myFunction() {
    document.getElementById("primaryNavigation").innerHTML = "Paragraph changed.";
     if (x.className === "show") {
         x.className = "hide";
     } else {
         x.className = "show";
+    }
