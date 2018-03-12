1ba6874b76b5dc0a80610ce424cfa72294ce8bbb
goubil <35411274+goubil@users.noreply.github.com>
2018-03-11 20:55:57 -0400

    update

--- assignments/franklin-7/scripts/windchill.js
+++ assignments/franklin-7/scripts/windchill.js
@@ -1,11 +1,36 @@
-var h = parseFloat(document.getElementById("hightemp").innerHTML);
-var l = parseFloat(document.getElementById("lowtemp").innerHTML);
-var avgtemp = (l + h) / 2;
+var urlString = document.location.href;
+var urlArray = urlString.split('/');
+var pageHREF = urlArray[urlArray.length - 1];
 
-var ws = parseFloat(document.getElementById("windspeed").innerHTML);
+if (pageHREF === 'franklin.php') {
+    var windspeed = 5;
+    var highTemp = 90;
+    var lowTemp = 66;
+    var calcWindChill = windChill(windspeed, highTemp, lowTemp);
+    document.getElementById("windChill").innerHTML = calcWindChill + "&deg;F";
+}
 
-var final = 35.74 + (0.6215 * avgtemp) - (35.75 * Math.pow(ws, 0.16)) + (0.4275 * avgtemp) * Math.pow(ws, 0.16);
+if (pageHREF === 'springfield.php') {
+    var windspeed = 15;
+    var highTemp = 72;
+    var lowTemp = 45;
+    var calcWindChill = windChill(windspeed, highTemp, lowTemp);
+    document.getElementById("windChill").innerHTML = calcWindChill + "&deg;F";
+}
 
-final = final.toFixed(2);
+if (pageHREF === 'greenville.php') {
+    var windspeed = 10;
+    var highTemp = 85;
+    var lowTemp = 60;
+    var calcWindChill = windChill(windspeed, highTemp, lowTemp);
+    document.getElementById("windChill").innerHTML = calcWindChill + "&deg;F";
+}
 
-document.getElementById("windchill").innerHTML = final;
+function windChill(windspeed, highTemp, lowTemp) {
+    "use strict";
+    var temp = (highTemp + lowTemp) / 2;
+    var s = Math.pow(windspeed, 0.16);
+    var calcWindChill = 35.74 + (0.6215 * temp) - (35.75 * s) + (0.4275 * temp * s);
+    calcWindChill = calcWindChill.toFixed(0);
+    return calcWindChill;
+}
