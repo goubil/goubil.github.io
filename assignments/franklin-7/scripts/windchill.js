1daaf8b062e448e32340b7762833dd8b9368955d
goubil <35411274+goubil@users.noreply.github.com>
2018-03-11 21:36:18 -0400

    update

--- assignments/franklin-7/scripts/windchill.js
+++ assignments/franklin-7/scripts/windchill.js
@@ -33,4 +33,6 @@ var t = (h + l) / 2;
 var s = 5;
 var f = 35.74 + 0.6215 * t - 35.75 * Math.pow(s, 0.16) + 0.4275 * t * Math.pow(s, 0.16);
 document.getElementById('f').innerHTML = Math.round(f) + "&deg;F";
+
+
 }
